57fd2fd394e60e3e7d61c650f169be759f0b5374
Alex <zhialex.ru@gmail.com>
2022-02-17 01:06:03 +0300

    upd

--- src/test/java/com/zhialex/ParameterizedWebTest.java
+++ src/test/java/com/zhialex/ParameterizedWebTest.java
@@ -1,13 +1,25 @@
 package com.zhialex;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ParameterizedWebTest {
 
+    static Stream<Arguments> commonSearchDataProvider() {
+        return Stream.of(
+                Arguments.of("Selenide", false, List.of("1", "2")),
+                Arguments.of("Junit", true, List.of("3", "4"))
+        );
+    }
+
+    //    @MethodSource("commonSearchDataProvider")
     @CsvSource(value = {
             "Selenide, Вышла Selenide",
             "JUnit, 5 is the next generation of"
--- src/test/java/com/zhialex/anime/SearchAnimeByNameTest.java
+++ src/test/java/com/zhialex/anime/SearchAnimeByNameTest.java
@@ -1,12 +1,23 @@
 package com.zhialex.anime;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchAnimeByNameTest extends BaseTest {
+
+    static Stream<Arguments> commonSearchDataProvider() {
+        return Stream.of(
+                Arguments.of("Naruto", "Это мой путь ниндзя!"),
+                Arguments.of("Steins;Gate", "Сняв в Акихабаре квартиру")
+        );
+    }
 
     @CsvSource(value = {
             "Naruto, Это мой путь ниндзя!",
@@ -14,6 +25,15 @@ public class SearchAnimeByNameTest extends BaseTest {
     })
     @ParameterizedTest(name = "Проверка, что находится нужное аниме : {0}")
     public void searchAnimeByNameTest(String animeName, String description) {
+        open("https://smotret-anime.online/");
+        $x("//div[@class='card']//a[text()='Поиск по названию']").click();
+        $(".row .m-big-search-input").setValue(animeName).pressEnter();
+        $$(".m-catalog-item__description").first().shouldHave(text(description));
+    }
+
+    @MethodSource("commonSearchDataProvider")
+    @ParameterizedTest(name = "Проверка, что находится нужное аниме")
+    public void searchAnimeByNameWithMethodSourceTest(String animeName, String description) {
         open("https://smotret-anime.online/");
         $x("//div[@class='card']//a[text()='Поиск по названию']").click();
         $(".row .m-big-search-input").setValue(animeName).pressEnter();
